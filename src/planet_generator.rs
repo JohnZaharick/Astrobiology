4462915cd6f69d30721049a650011235a469bb00
John Zaharick <jzaharic@umn.edu>
2023-02-27 15:09:36 -0600

    Star struct is borrowed by planet_generator so it can influence planet generation.

--- src/planet_generator.rs
+++ src/planet_generator.rs
@@ -2,26 +2,39 @@ use strum_macros::Display;
 use rand::prelude::*;
 use rand_derive2::RandGen;
 
+use crate::star_generator;
+
 #[derive(Display, RandGen)]
 pub enum PlanetClass{
     Rocky, GasGiant, IceGiant, Dwarf,
 }
 
+// temperature;
+// radiation;
+// pressure;
+// mass;
+// magneticField;
+// isHabitable;
+// liquid;
 pub struct Planet {
     pub class: PlanetClass,
+    pub distance_from_star: u8,
+    pub habitable: bool,
 }
 
 impl Planet {
-    fn new() -> Planet{
+    fn new(star: &star_generator::Star, distance: u8) -> Planet{
         Planet {
             class: random(),
+            distance_from_star: distance,
+            habitable: if star.age > 1000 {true} else {false},
         }
     }
 
-    pub fn planets(count: u8) -> Vec<Planet> {
+    pub fn planets(star: &star_generator::Star, count: u8) -> Vec<Planet> {
         let mut planets = Vec::new();
-        for _temp in 1..count{
-            planets.push(Planet::new());
+        for i in 1..count{
+            planets.push(Planet::new(star,i));
         }
         planets
     }
--- src/planetary_system_generator.rs
+++ src/planetary_system_generator.rs
@@ -7,24 +7,24 @@ const PLANET_NUMBER_MINIMUM: u8 = 3;
 const PLANET_NUMBER_MAXIMUM: u8 = 12;
 
 struct PlanetarySystem {
-    star: star_generator::Star,
     planets: Vec<planet_generator::Planet>,
+    star: star_generator::Star,
 }
 
 impl PlanetarySystem {
-    fn new() -> Self {
+    fn new(star: star_generator::Star) -> Self {
         let mut rng = rand::thread_rng();
 
         PlanetarySystem {
-            star: star_generator::Star::new(),
-            planets: planet_generator::Planet::planets(
+            planets: planet_generator::Planet::planets(&star,
                 rng.gen_range(PLANET_NUMBER_MINIMUM..=PLANET_NUMBER_MAXIMUM)),
+            star,
         }
     }
 }
 
 pub fn explore_system() -> String {
-    let solar_system = PlanetarySystem::new();
+    let solar_system = PlanetarySystem::new(star_generator::Star::new());
     format!("There is an {} star that is {}, {} million years old, weighs {} M☉, and is {} K. \
             It has {} planets.", solar_system.star.class, solar_system.star.color,
             solar_system.star.age, solar_system.star.mass, solar_system.star.temperature,
